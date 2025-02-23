39d8a53bff461e63f836f907577d7514826366c0
Robert Schneider <robert.schneider@stud.fh-campuswien.ac.at>
2025-02-23 16:53:35 +0100

    Genre shenanigans

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,16 +1,19 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
     private String title;
     private String description;
+    private List<Genre> genres;
     // TODO add more properties here
 
-    public Movie(String title, String description) {
+    public Movie(String title, String description, List<Genre> genres) {
         this.title = title;
         this.description = description;
+        this.genres = new ArrayList<>(genres);
     }
 
     public String getTitle() {
@@ -21,8 +24,11 @@ public class Movie {
         return description;
     }
 
+    public List<Genre> getGenres() { return List.copyOf(genres); }
+
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("title", "description", List.of(Genre.ACTION, Genre.DRAMA, Genre.DOCUMENTARY)));
         // TODO add some dummy data here
 
         return movies;
