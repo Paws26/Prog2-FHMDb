c293ebef290ba5ed17633ff0d31597175655529c
LukasP <pamperl.lukas@gmail.com>
2025-04-03 16:29:47 +0200

    changed genre enum "NONE" to "ANY"

--- src/main/java/at/ac/fhcampuswien/fhmdb/helpers/MovieDisplayHelper.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/helpers/MovieDisplayHelper.java
@@ -132,7 +132,7 @@ public class MovieDisplayHelper {
 
     //Filter the incoming List of Movies by the genre
     public static List<Movie> filterMoviesByGenre(List<Movie> moviesToFilter, Genre genre) {
-        if (genre == Genre.NONE || genre == null) {
+        if (genre == Genre.ANY || genre == null) {
             return moviesToFilter;
         }
         List<Movie> filteredMovies = new ArrayList<>();
