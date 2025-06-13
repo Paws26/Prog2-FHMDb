839cbf856de77cb4369a4bd44aa74398df512334
LukasP <pamperl.lukas@gmail.com>
2025-06-13 16:01:45 +0200

    removed ormlite-core dependency, re-added module-info.java, changed runConfiguration accordingly

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module at.ac.fhcampuswien.fhmdb {
+    requires javafx.fxml;
+    requires com.jfoenix;
+    requires javafx.controls;
+    requires okhttp3;
+    requires com.google.gson;
+    requires surefire.shared.utils;
+    requires ormlite.jdbc;
+    requires java.sql;
+
+    opens at.ac.fhcampuswien.fhmdb.database to ormlite.jdbc;
+
+    opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
+    opens at.ac.fhcampuswien.fhmdb.models to com.google.gson, ormlite.jdbc;
+    exports at.ac.fhcampuswien.fhmdb;
+    exports at.ac.fhcampuswien.fhmdb.models;
+
+    requires static lombok;
+
+}
